362c3155d7878efb71fa5cec4dcdcb38c0b55b9a
RonitGH <ronitharouni@gmail.com>
2026-08-02 14:04:27 +0300

    Rename source table to source_config and fix data types

--- sql/001_Create_Control_Tables.sql
+++ sql/001_Create_Control_Tables.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS
-  `control.source_systems`
+`control.source_config`
 (
-  source_id STRING NOT NULL,
-  source_name STRING NOT NULL,
-  source_type STRING NOT NULL,
-  source_url STRING,
-  landing_path STRING NOT NULL,
-  raw_table STRING NOT NULL,
-  load_type STRING NOT NULL,
-  is_active BOOL NOT NULL,
-  last_successful_load TIMESTAMP,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+    source_id INT64 NOT NULL,
+    source_name STRING NOT NULL,
+    source_type STRING NOT NULL,
+    source_url STRING,
+    landing_path STRING NOT NULL,
+    raw_table STRING NOT NULL,
+    load_type STRING NOT NULL,
+    is_active BOOL NOT NULL,
+    last_successful_load TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 );
